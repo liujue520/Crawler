242fc851744dde21f35dc0d998d7c6b4bd4e0e69
刘爵 <2279397057@qq.com>
2017-08-23 11:42:33 +0800

    Update jiaoben.py

--- jiaoben.py
+++ jiaoben.py
@@ -1,10 +1,3 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: loveNight
-# @Date: 2015-10-28 19:59:24
-# @Last Modified by: loveNight
-# @Last Modified time: 2015-11-15 19:24:57
-
 import urllib
 import requests
 import os
@@ -18,8 +11,6 @@ from multiprocessing import Queue
 
 
 class BaiduImgDownloader(object):
-    """百度图片下载工具，目前只支持单个关键词"""
-    # 解码网址用的映射表
     str_table = {
     '_z2C$q': ':',
     '_z&e3B': '.',
@@ -87,14 +78,13 @@ class BaiduImgDownloader(object):
                 self.session.headers = BaiduImgDownloader.headers
                 self.queue = Queue()
                 self.messageQueue = Queue()
-                self.index = 0  # 图片起始编号，牵涉到计数，不要更改
-                self.promptNum = 10  # 下载几张图片提示一次
+                self.index = 0  
+                self.promptNum = 10 
                 self.lock = threading.Lock()
-                self.delay = 1.5  # 网络请求太频繁会被封
-                self.QUIT = "QUIT"  # Queue中表示任务结束
-                self.printPrefix = "**"  # 用于指定在控制台输出
-
-    # 控制台输出线程
+                self.delay = 1.5  
+                self.QUIT = "QUIT" 
+                self.printPrefix = "**"  
+                
     def start(self):
         t = threading.Thread(target=self.__log)
         t.setDaemon(True)
@@ -102,8 +92,7 @@ class BaiduImgDownloader(object):
         self.messageQueue.put(self.printPrefix + "脚本开始执行")
         start_time = dt.now()
         urls = self.__buildUrls()
-        self.messageQueue.put(self.printPrefix + "已获取 %s 个Json请求网址" % len(urls))
-        # 解析出所有图片网址，该方法会阻塞直到任务完成
+        self.messageQueue.put(self.printPrefix + "已获取 %s 个Json请求网址" % len(urls)
         self.pool.map(self.__resolveImgUrl, urls)
         while self.queue.qsize():
             imgs = self.queue.get()
@@ -115,8 +104,7 @@ class BaiduImgDownloader(object):
         self.messageQueue.put(self.printPrefix + "请到 %s 查看结果！" % self.dirpath)
         self.messageQueue.put(self.printPrefix + "错误信息保存在 %s" % self.errorFile)
         self.messageQueue.put(self.QUIT)
-
-    #控制台输出，加锁以免被多线程打乱
+                              
     def __log(self):
         with open(self.logFile, "w", encoding="utf-8") as f:
             while True:
@@ -127,8 +115,6 @@ class BaiduImgDownloader(object):
             if self.printPrefix in message:
                 print(message)
                 elif "已下载" in message:
-                # 下载N张图片提示一次
-                # downNum = self.re_downNum.findall(message)
             if downNum and int(downNum[0]) % self.promptNum == 0:
                 print(message)
                 f.write(message + '\n')
@@ -136,9 +122,6 @@ class BaiduImgDownloader(object):
 
 
 def __getIndex(self):
-    """获取文件编号"""
-
-
 self.lock.acquire()
 try:
     return self.index
@@ -148,25 +131,16 @@ self.lock.release()
 
 
 def decode(self, url):
-    """解码图片URL
-    解码前：
     ippr_z2C$qAzdH3FAzdH3Ffl_z&e3Bftgwt42_z&e3BvgAzdH3F4omlaAzdH3Faa8W3ZyEpymRmx3Y1p7bb&mla
-    解码后：
     http://s9.sinaimg.cn/mw690/001WjZyEty6R6xjYdtu88&690
     """
-
-
-# 先替换字符串
+    
 for key, value in self.str_table.items():
     url = url.replace(key, value)
-# 再替换剩下的字符
 return url.translate(self.char_table)
-
 
 def __buildUrls(self):
     """json请求网址生成器"""
-
-
 word = urllib.parse.quote(self.word)
 url = r"http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&fp=result&queryWord={word}&cl=2&lm=-1&ie=utf-8&oe=utf-8&st=-1&ic=0&word={word}&face=0&istype=2nc=1&pn={pn}&rn=60"
 time.sleep(self.delay)
@@ -180,11 +154,9 @@ with open(self.jsonUrlFile, "w", encoding="utf-8") as f:
         f.write(url + "\n")
 return urls
 
-
 def __resolveImgUrl(self, url):
     """从指定网页中解析出图片URL"""
-
-
+    
 time.sleep(self.delay)
 html = self.session.get(url, timeout=15).content.decode('utf-8')
 datas = self.re_objURL.findall(html)
@@ -192,11 +164,9 @@ imgs = [Image(self.decode(x[0]), x[1]) for x in datas]
 self.messageQueue.put(self.printPrefix + "已解析出 %s 个图片网址" % len(imgs))
 self.queue.put(imgs)
 
-
 def __downImg(self, img):
     """下载单张图片，传入的是Image对象"""
-
-
+    
 imgUrl = img.url
 # self.messageQueue.put("线程 %s 正在下载 %s " %
 # (threading.current_thread().name, imgUrl))
@@ -222,10 +192,8 @@ filename = os.path.join(self.dirpath, str(index) + "." + img.type)
 with open(filename, "wb") as f:
     f.write(res.content)
 
-
 def __saveError(self, message):
     self.lock.acquire()
-
 
 try:
     with open(self.errorFile, "a", encoding="utf-8") as f:
@@ -233,16 +201,12 @@ try:
 finally:
     self.lock.release()
 
-
 class Image(object):
-
 
 """图片类，保存图片信息"""
 
-
 def __init__(self, url, type):
     super(Image, self).__init__()
-
 
 self.url = url
 self.type = type
